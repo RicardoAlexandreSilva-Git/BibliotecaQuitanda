bcb093a5a6cd5a4b9fd646dff127c7003860859b
RicardoAlexandre <ricardo95alexandre@gmail.com>
2026-06-04 10:48:14 -0300

    ClienteDAO atualizado

--- src/dao/ClienteDAO.java
+++ src/dao/ClienteDAO.java
@@ -1,4 +1,26 @@
 package dao;
 
+import model.Cliente;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ClienteDAO {
+    private List<Cliente> clientes= new ArrayList<>();
+
+    public void cadastrarCliente(Cliente cliente){
+        clientes.add(cliente);
+    }
+
+    public Cliente buscarCliente(String cpf){
+        return clientes.stream().filter(c->c.getCpf().equals(cpf)).findFirst().orElse(null);
+    }
+
+    public void listarClientes(){
+        clientes.forEach(System.out::println);
+    }
+
+    public void removerCliente(String cpf){
+        clientes.removeIf(c->c.getCpf().equals(cpf));
+    }
 }
