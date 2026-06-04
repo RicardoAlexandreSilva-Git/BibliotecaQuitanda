3ddfc10c0fca6d9cf1507ada296ce8203a3dadc8
RicardoAlexandre <ricardo95alexandre@gmail.com>
2026-06-04 10:37:47 -0300

    Inserido Models base.

--- src/model/Livro.java
+++ src/model/Livro.java
@@ -1,4 +1,34 @@
 package model;
 
 public class Livro {
+    private String nomeLivro;
+    private String codigoLivro;
+    private int estoqueLivro;
+
+    public Livro(String nomeLivro, String codigoLivro, int estoqueLivro){
+        this.nomeLivro=nomeLivro;
+        this.codigoLivro=codigoLivro;
+        this.estoqueLivro=estoqueLivro;
+    }
+
+    public String getNomeLivro() {
+        return nomeLivro;
+    }
+
+    public String getCodigoLivro() {
+        return codigoLivro;
+    }
+
+    public int getEstoqueLivro() {
+        return estoqueLivro;
+    }
+
+    public void setEstoqueLivro(int estoqueLivro) {
+        this.estoqueLivro = estoqueLivro;
+    }
+
+    @Override
+    public String toString(){
+        return "Livro: "+ nomeLivro+" | Código: "+ codigoLivro+" | Estoque: "+estoqueLivro;
+    }
 }
